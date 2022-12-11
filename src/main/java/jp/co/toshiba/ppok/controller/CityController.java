8c62da5a2f46bb31442b0334b37ff9bff3bc980a
ArcaHozota <arkamahozota@gmail.com>
2022-12-11 22:18:06 +0900

    1.26

--- src/main/java/jp/co/toshiba/ppok/controller/CityController.java
+++ src/main/java/jp/co/toshiba/ppok/controller/CityController.java
@@ -4,8 +4,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import javax.annotation.Resource;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -33,96 +32,96 @@ import jp.co.toshiba.ppok.utils.RestMsg;
 @RestController
 public class CityController {
 
-	@Resource
-	private CityDtoService cityDtoService;
+    @Autowired
+    private CityDtoService cityDtoService;
 
-	/**
-	 * Retrieve the city data.
-	 *
-	 * @return page(JSON)
-	 */
-	@GetMapping(value = "/city")
-	public RestMsg getCities(@RequestParam(value = "pageNum", defaultValue = "1") final Integer pageNum) {
-		PageMethod.startPage(pageNum, 12);
-		final List<CityDto> list = cityDtoService.getAll();
-		final PageInfo<CityDto> pageInfo = new PageInfo<>(list, 7);
-		return RestMsg.success().add("pageInfo", pageInfo);
-	}
+    /**
+     * Retrieve the city data.
+     *
+     * @return page(JSON)
+     */
+    @GetMapping(value = "/city")
+    public RestMsg getCities(@RequestParam(value = "pageNum", defaultValue = "1") final Integer pageNum) {
+        PageMethod.startPage(pageNum, 12);
+        final List<CityDto> list = cityDtoService.getAll();
+        final PageInfo<CityDto> pageInfo = new PageInfo<>(list, 7);
+        return RestMsg.success().add("pageInfo", pageInfo);
+    }
 
-	/**
-	 * Save the input messages.
-	 *
-	 * @param cityDto the input message of cities
-	 * @return RestMsg.success();
-	 */
-	@PostMapping(value = "/city")
-	public RestMsg saveCityInfos(@Valid final CityDto cityDto, final BindingResult result) {
-		final Map<String, Object> map = new HashMap<>(5);
-		if (result.hasErrors()) {
-			final List<FieldError> fieldErrors = result.getFieldErrors();
-			for (final FieldError fieldError : fieldErrors) {
-				map.put(fieldError.getField(), fieldError.getDefaultMessage());
-			}
-			return RestMsg.failure().add("errorFields", map);
-		} else {
-			cityDtoService.saveCityInfo(cityDto);
-			return RestMsg.success();
-		}
-	}
+    /**
+     * Save the input messages.
+     *
+     * @param cityDto the input message of cities
+     * @return RestMsg.success();
+     */
+    @PostMapping(value = "/city")
+    public RestMsg saveCityInfos(@Valid final CityDto cityDto, final BindingResult result) {
+        final Map<String, Object> map = new HashMap<>(5);
+        if (result.hasErrors()) {
+            final List<FieldError> fieldErrors = result.getFieldErrors();
+            for (final FieldError fieldError : fieldErrors) {
+                map.put(fieldError.getField(), fieldError.getDefaultMessage());
+            }
+            return RestMsg.failure().add("errorFields", map);
+        } else {
+            cityDtoService.saveCityInfo(cityDto);
+            return RestMsg.success();
+        }
+    }
 
-	/**
-	 * Check the input city name already existed or not.
-	 *
-	 * @param cityName the input name
-	 * @return RestMsg.success()
-	 */
-	@GetMapping(value = "/checklist")
-	public RestMsg checkCityName(@RequestParam("cityName") final String cityName) {
-		final String regex = "^[a-zA-Z_-]{4,17}$";
-		if (cityName.matches(regex)) {
-			if (cityDtoService.checkDuplicated(cityName)) {
-				return RestMsg.failure().add("validatedMsg", "City name is duplicate.");
-			} else {
-				return RestMsg.success();
-			}
-		} else {
-			return RestMsg.failure().add("validatedMsg", "Name of cities should be in 4~17 Latin alphabets.");
-		}
-	}
+    /**
+     * Check the input city name already existed or not.
+     *
+     * @param cityName the input name
+     * @return RestMsg.success()
+     */
+    @GetMapping(value = "/checklist")
+    public RestMsg checkCityName(@RequestParam("cityName") final String cityName) {
+        final String regex = "^[a-zA-Z_-]{4,17}$";
+        if (cityName.matches(regex)) {
+            if (cityDtoService.checkDuplicated(cityName)) {
+                return RestMsg.failure().add("validatedMsg", "City name is duplicate.");
+            } else {
+                return RestMsg.success();
+            }
+        } else {
+            return RestMsg.failure().add("validatedMsg", "Name of cities should be in 4~17 Latin alphabets.");
+        }
+    }
 
-	/**
-	 * Search the selected city's name.
-	 *
-	 * @param id the ID of city
-	 * @return RestMsg.success().add(data)
-	 */
-	@GetMapping(value = "/city/{id}")
-	public RestMsg getCityName(@PathVariable("id") final Long id) {
-		final CityDto city = cityDtoService.getCityInfo(id);
-		return RestMsg.success().add("citySelected", city);
-	}
+    /**
+     * Search the selected city's name.
+     *
+     * @param id the ID of city
+     * @return RestMsg.success().add(data)
+     */
+    @GetMapping(value = "/city/{id}")
+    public RestMsg getCityName(@PathVariable("id") final Long id) {
+        final CityDto city = cityDtoService.getCityInfo(id);
+        return RestMsg.success().add("citySelected", city);
+    }
 
-	/**
-	 * Update city info.
-	 *
-	 * @param cityDto the input message of cities
-	 * @return RestMsg.success()
-	 */
-	@PutMapping(value = "/city/{id}")
-	public RestMsg saveCityChanges(@RequestBody final CityDto cityDto) {
-		cityDtoService.updateCityInfo(cityDto);
-		return RestMsg.success();
-	}
+    /**
+     * Update city info.
+     *
+     * @param cityDto the input message of cities
+     * @return RestMsg.success()
+     */
+    @PutMapping(value = "/city/{id}")
+    public RestMsg saveCityChanges(@RequestBody final CityDto cityDto) {
+        cityDtoService.updateCityInfo(cityDto);
+        return RestMsg.success();
+    }
 
-	/**
-	 * Delete the selected city info.
-	 *
-	 * @param id the ID of city
-	 * @return RestMsg.success()
-	 */
-	@DeleteMapping(value = "/city/{id}")
-	public RestMsg deleteCityInfo(@PathVariable("id") final Long id) {
-		cityDtoService.deleteCityInfo(id);
-		return RestMsg.success();
-	}
+    /**
+     * Delete the selected city info.
+     *
+     * @param id the ID of city
+     * @return RestMsg.success()
+     */
+    @DeleteMapping(value = "/city/{id}")
+    public RestMsg deleteCityInfo(@PathVariable("id") final Long id) {
+        cityDtoService.deleteCityInfo(id);
+        return RestMsg.success();
+    }
 }
