5aa33a5bda13342d0cf89e4b1c063b0d1739697d
ArcaHozota <arkamahozota@gmail.com>
2022-12-11 17:50:37 +0900

    1.22c

--- src/main/java/jp/co/toshiba/ppok/mapper/CityDao.java
+++ src/main/java/jp/co/toshiba/ppok/mapper/CityDao.java
@@ -2,17 +2,19 @@ package jp.co.toshiba.ppok.mapper;
 
 import java.util.List;
 
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import jp.co.toshiba.ppok.utils.CityDto;
 import org.apache.ibatis.annotations.Mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+import jp.co.toshiba.ppok.utils.CityDto;
 
 @Mapper
 public interface CityDao extends BaseMapper<CityDto> {
 
-    /**
-     * Search the cities in wcv.
-     *
-     * @return List<City>
-     */
-    List<CityDto> selectByCityView();
+	/**
+	 * Search the cities in wcv.
+	 *
+	 * @return List<City>
+	 */
+	List<CityDto> selectByCityView();
 }
