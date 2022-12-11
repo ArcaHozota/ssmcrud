be9d4818cd3d4de1a1cda2d6de9a873f84babbc6
ArcaHozota <arkamahozota@gmail.com>
2022-12-11 17:31:03 +0900

    1.19a

--- src/main/java/jp/co/toshiba/ppok/entity/City.java
+++ src/main/java/jp/co/toshiba/ppok/entity/City.java
@@ -2,41 +2,41 @@ package jp.co.toshiba.ppok.entity;
 
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableLogic;
-import jakarta.validation.constraints.Pattern;
+
 import lombok.Data;
 
 @Data
 public class City {
 
-    /**
-     * This field corresponds to the database column WORLD_CITY.ID
-     */
-    @TableId
-    private Long id;
+	/**
+	 * This field corresponds to the database column WORLD_CITY.ID
+	 */
+	@TableId
+	private Long id;
 
-    /**
-     * This field corresponds to the database column WORLD_CITY.NAME
-     */
-    private String name;
+	/**
+	 * This field corresponds to the database column WORLD_CITY.NAME
+	 */
+	private String name;
 
-    /**
-     * This field corresponds to the database column WORLD_CITY.COUNTRY_CODE
-     */
-    private String countryCode;
+	/**
+	 * This field corresponds to the database column WORLD_CITY.COUNTRY_CODE
+	 */
+	private String countryCode;
 
-    /**
-     * This field corresponds to the database column WORLD_CITY.DISTRICT
-     */
-    private String district;
+	/**
+	 * This field corresponds to the database column WORLD_CITY.DISTRICT
+	 */
+	private String district;
 
-    /**
-     * This field corresponds to the database column WORLD_CITY.POPULATION
-     */
-    private Long population;
+	/**
+	 * This field corresponds to the database column WORLD_CITY.POPULATION
+	 */
+	private Long population;
 
-    /**
-     * This field corresponds to the database column WORLD_CITY.IS_DELETED
-     */
-    @TableLogic
-    private Integer isDeleted;
+	/**
+	 * This field corresponds to the database column WORLD_CITY.IS_DELETED
+	 */
+	@TableLogic
+	private Integer isDeleted;
 }
