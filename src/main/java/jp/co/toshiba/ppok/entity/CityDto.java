013eb22b27122b5627cb54506c53d305b195f790
ArcaHozota <arkamahozota@gmail.com>
2022-12-12 10:58:41 +0900

    1.39

---
+++ src/main/java/jp/co/toshiba/ppok/entity/CityDto.java
@@ -0,0 +1,45 @@
+package jp.co.toshiba.ppok.entity;
+
+import jakarta.validation.constraints.Pattern;
+import lombok.Data;
+
+/**
+ * dto of the view of world cities
+ *
+ * @author Administrator
+ */
+
+@Data
+public class CityDto {
+
+	/**
+	 * This field corresponds to the database column WORLD_CITY_VIEW.ID
+	 */
+	private Long id;
+
+	/**
+	 * This field corresponds to the database column WORLD_CITY_VIEW.NAME
+	 */
+	@Pattern(regexp = "^[a-zA-Z_-]{4,17}$", message = "Name of cities should be in 4~17 Latin alphabets.")
+	private String name;
+
+	/**
+	 * This field corresponds to the database column WORLD_CITY_VIEW.CONTINENT
+	 */
+	private String continent;
+
+	/**
+	 * This field corresponds to the database column WORLD_CITY_VIEW.NATION
+	 */
+	private String nation;
+
+	/**
+	 * This field corresponds to the database column WORLD_CITY_VIEW.DISTRICT
+	 */
+	private String district;
+
+	/**
+	 * This field corresponds to the database column WORLD_CITY_VIEW.POPULATION
+	 */
+	private Long population;
+}
