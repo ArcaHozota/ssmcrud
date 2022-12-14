4eba2abff7d116d893fc60409768ec78582eda8b
ArcaHozota <arkamahozota@gmail.com>
2022-12-14 15:08:46 +0900

    2.30

--- src/main/java/jp/co/toshiba/ppok/config/WebMvcConfig.java
+++ src/main/java/jp/co/toshiba/ppok/config/WebMvcConfig.java
@@ -40,6 +40,7 @@ public class WebMvcConfig extends WebMvcConfigurationSupport {
     @Override
     protected void addResourceHandlers(ResourceHandlerRegistry registry) {
         log.info("静的リソースのマッピングが開始しました。");
+        registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
         registry.addResourceHandler("/jquery/**").addResourceLocations("classpath:/static/jquery/");
         registry.addResourceHandler("/bootstrap/**").addResourceLocations("classpath:/static/bootstrap/");
     }
