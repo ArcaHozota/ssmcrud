9f019d8273bb7d1a01d42dc8d0961a8c65136ff3
ArcaHozota <arkamahozota@gmail.com>
2022-12-15 15:45:29 +0900

    2.79

--- src/main/java/jp/co/toshiba/ppok/controller/ForwardController.java
+++ src/main/java/jp/co/toshiba/ppok/controller/ForwardController.java
@@ -15,6 +15,6 @@ public class ForwardController {
 
     @RequestMapping("/index")
     public void index(HttpServletResponse response) throws IOException {
-        response.sendRedirect("/index.html");
+        response.sendRedirect("/public/cities.html");
     }
 }
