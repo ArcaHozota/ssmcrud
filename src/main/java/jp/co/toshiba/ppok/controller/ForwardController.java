2ad79139efd3aaef4de70d90a738be4a3958cf23
ArcaHozota <arkamahozota@gmail.com>
2022-12-15 14:55:37 +0900

    2.64

---
+++ src/main/java/jp/co/toshiba/ppok/controller/ForwardController.java
@@ -0,0 +1,17 @@
+package jp.co.toshiba.ppok.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Controller
+public class ForwardController {
+
+    @RequestMapping("/grssmcrud/city")
+    public void index(HttpServletResponse response) throws IOException {
+        response.sendRedirect("/public/cities.html");
+    }
+}
