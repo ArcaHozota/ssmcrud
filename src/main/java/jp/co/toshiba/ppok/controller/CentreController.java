26935bd54d78198aaa5dbca9514fcc0d9636aba2
ArcaHozota <arkamahozota@gmail.com>
2022-12-15 15:45:24 +0900

    2.78

--- src/main/java/jp/co/toshiba/ppok/controller/CentreController.java
+++ src/main/java/jp/co/toshiba/ppok/controller/CentreController.java
@@ -24,7 +24,7 @@ import javax.annotation.Resource;
  * @author Administrator
  */
 @RestController
-@RequestMapping("/grssmcrud")
+@RequestMapping("/public/grssmcrud")
 public class CentreController {
 
     @Resource
