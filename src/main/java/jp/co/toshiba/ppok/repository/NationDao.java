41b20a3b90f9cb96f121470f3a11f3cd992963db
ArcaHozota <arkamahozota@gmail.com>
2023-01-02 02:13:32 +0900

    5.27

--- src/main/java/jp/co/toshiba/ppok/repository/NationDao.java
+++ src/main/java/jp/co/toshiba/ppok/repository/NationDao.java
@@ -2,7 +2,11 @@ package jp.co.toshiba.ppok.repository;
 
 import jp.co.toshiba.ppok.entity.Nation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 /**
  * searching dao of table WORLD_COUNTRY
@@ -12,4 +16,13 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public interface NationDao extends JpaRepository<Nation, String> {
+
+    /**
+     * Retrieve the nation list distinct.
+     *
+     * @param continent name of continent
+     * @return List<CityInfo>
+     */
+    @Query(value = "select distinct new Nation(na.name) from Nation na where na.continent =:continent")
+    List<Nation> getNations(@Param("continent") final String continent);
 }
