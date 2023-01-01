8aa4b7244bc022a3527ee54d420f043ae676a37a
ArcaHozota <arkamahozota@gmail.com>
2023-01-02 02:46:09 +0900

    5.35

--- src/main/java/jp/co/toshiba/ppok/repository/CityDao.java
+++ src/main/java/jp/co/toshiba/ppok/repository/CityDao.java
@@ -2,6 +2,7 @@ package jp.co.toshiba.ppok.repository;
 
 import jp.co.toshiba.ppok.entity.City;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -20,6 +21,7 @@ public interface CityDao extends JpaRepository<City, Long> {
      *
      * @param id id of the selected city
      */
+    @Modifying
     @Query(value = "update City cvn set cvn.isDeleted = 1 where cvn.id =:id")
     void removeById(@Param("id") final Long id);
 }
