ca6ee2edb095e71f0fa9da8e571ee4bd8d62372f
ArcaHozota <arkamahozota@gmail.com>
2023-01-01 21:26:56 +0900

    4.94

--- src/main/java/jp/co/toshiba/ppok/repository/CityInfoDao.java
+++ src/main/java/jp/co/toshiba/ppok/repository/CityInfoDao.java
@@ -18,19 +18,11 @@ import java.util.List;
 public interface CityInfoDao extends JpaRepository<CityInfo, Long> {
 
 	/**
-	 * retrieve the continent list distinct.
-	 * 
-	 * @return List<CityInfo>
-	 */
-	@Query(value = "select distinct new CityInfo(cvw.continent) from CityInfo cvw")
-	List<CityInfo> getContinents();
-
-	/**
 	 * retrieve the nation list distinct.
 	 * 
 	 * @param continent name of continent
 	 * @return List<CityInfo>
 	 */
-	@Query(value = "select distinct new CityInfo(cvw.name,cvw.nation) from CityInfo cvw where cvw.continent =:continent")
-	List<CityInfo> getNations(@Param("continent") final String continent);
+	@Query(value = "select distinct new CityInfo(cvw.nation) from CityInfo cvw where cvw.continent =:continent")
+	List<String> getNations(@Param("continent") final String continent);
 }
