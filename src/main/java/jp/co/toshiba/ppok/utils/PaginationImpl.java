f8ccd485d744aad940c44c3f204000b2e7b41948
ArcaHozota <arkamahozota@gmail.com>
2023-01-01 11:53:28 +0900

    4.59

--- src/main/java/jp/co/toshiba/ppok/utils/PaginationImpl.java
+++ src/main/java/jp/co/toshiba/ppok/utils/PaginationImpl.java
@@ -3,6 +3,7 @@ package jp.co.toshiba.ppok.utils;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 
+import javax.annotation.PostConstruct;
 import java.util.List;
 
 /**
@@ -13,6 +14,19 @@ import java.util.List;
  * @author Mark Paluch
  */
 public class PaginationImpl<T> extends PageImpl<T> {
+
+    private Integer current;
+
+    private Integer totalPg;
+
+    private Long totalRecords;
+
+    @PostConstruct
+    protected void init(){
+        this.current = super.getNumber();
+        this.totalPg = super.getTotalPages();
+        this.totalRecords = super.getTotalElements();
+    }
 
     public PaginationImpl(List<T> content, Pageable pageable, long total) {
         super(content, pageable, total);
