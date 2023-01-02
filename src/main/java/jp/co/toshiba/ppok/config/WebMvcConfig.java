04f63f57b0295cc173cb46a9433e4d48c545986d
ArcaHozota <arkamahozota@gmail.com>
2023-01-02 13:59:36 +0900

    5.40

--- src/main/java/jp/co/toshiba/ppok/config/WebMvcConfig.java
+++ src/main/java/jp/co/toshiba/ppok/config/WebMvcConfig.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
@@ -15,6 +16,7 @@ import java.util.List;
  */
 @Slf4j
 @Configuration
+@EnableWebMvc
 public class WebMvcConfig extends WebMvcConfigurationSupport {
 
     /**
