3a7594df29bd45b5b3f23816613407c32669906a
ArcaHozota <arkamahozota@gmail.com>
2023-01-06 02:06:03 +0900

    5.54

--- src/main/java/jp/co/toshiba/ppok/utils/StringUtils.java
+++ src/main/java/jp/co/toshiba/ppok/utils/StringUtils.java
@@ -11,34 +11,6 @@ import org.springframework.lang.Nullable;
 public class StringUtils extends org.springframework.util.StringUtils {
 
 	/**
-	 * 兩者相等
-	 *
-	 * @param str1 值
-	 * @param str2 值
-	 * @return 判斷結果
-	 */
-	public static boolean isEqual(final String str1, final String str2) {
-		boolean isEqual = false;
-		if (!str1.isBlank() || !str2.isBlank()) {
-			isEqual = str1.equals(str2);
-		} else if (str1.isBlank() && str2.isBlank()) {
-			isEqual = (str1.length() == str2.length());
-		}
-		return isEqual;
-	}
-
-	/**
-	 * 兩者不等
-	 *
-	 * @param str1 值
-	 * @param str2 值
-	 * @return 判斷結果
-	 */
-	public static boolean isNotEqual(final String str1, final String str2) {
-		return !isEqual(str1, str2);
-	}
-
-	/**
 	 * 判斷該字符串是否爲空
 	 *
 	 * @param str 目標字符串
@@ -58,4 +30,31 @@ public class StringUtils extends org.springframework.util.StringUtils {
 		return !isEmpty(str);
 	}
 
+	/**
+	 * 兩者相等
+	 *
+	 * @param str1 值
+	 * @param str2 值
+	 * @return 判斷結果
+	 */
+	public static boolean isEqual(@Nullable final String str1, @Nullable final String str2) {
+		boolean isEqual = false;
+		if (!str1.isBlank() || !str2.isBlank()) {
+			isEqual = str1.equals(str2);
+		} else if (str1.isBlank() && str2.isBlank()) {
+			isEqual = (str1.length() == str2.length());
+		}
+		return isEqual;
+	}
+
+	/**
+	 * 兩者不等
+	 *
+	 * @param str1 值
+	 * @param str2 值
+	 * @return 判斷結果
+	 */
+	public static boolean isNotEqual(@Nullable final String str1, @Nullable final String str2) {
+		return !isEqual(str1, str2);
+	}
 }
