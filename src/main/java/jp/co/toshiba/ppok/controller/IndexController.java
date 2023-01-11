4670db1155772cc160c4cec0cb70555a8b05ba08
ArcaHozota <arkamahozota@gmail.com>
2023-01-11 14:44:57 +0900

    5.93

--- src/main/java/jp/co/toshiba/ppok/controller/IndexController.java
+++ src/main/java/jp/co/toshiba/ppok/controller/IndexController.java
@@ -2,8 +2,7 @@ package jp.co.toshiba.ppok.controller;
 
 import java.io.IOException;
 
-import javax.servlet.http.HttpServletResponse;
-
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
