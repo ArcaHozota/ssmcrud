078246bca4cb212c10f9491daef3fcd796afbd95
kyut <kyut@kyut-virtual-machine>
2023-01-11 16:40:47 +0900

    6.07

--- src/main/java/jp/co/toshiba/ppok/controller/IndexController.java
+++ src/main/java/jp/co/toshiba/ppok/controller/IndexController.java
@@ -2,7 +2,8 @@ package jp.co.toshiba.ppok.controller;
 
 import java.io.IOException;
 
-import jakarta.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
