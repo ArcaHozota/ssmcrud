d9950163a65d855a5e32d3b3371fd3a885ade4c6
ArcaHozota <arkamahozota@gmail.com>
2023-01-13 11:29:05 +0900

    6.16

--- src/main/java/jp/co/toshiba/ppok/utils/RestMsg.java
+++ src/main/java/jp/co/toshiba/ppok/utils/RestMsg.java
@@ -1,9 +1,9 @@
 package jp.co.toshiba.ppok.utils;
 
-import lombok.Data;
-
 import java.util.HashMap;
 import java.util.Map;
+
+import lombok.Data;
 
 /**
  * The common class of JSON-data response.
@@ -13,60 +13,54 @@ import java.util.Map;
 @Data
 public class RestMsg {
 
-    /**
-     * status code
-     */
-    private Integer code;
+	/**
+	 * status code
+	 */
+	private Integer code;
 
-    /**
-     * the message of status
-     */
-    private String message;
+	/**
+	 * the message of status
+	 */
+	private String message;
 
-    /**
-     * data returned to browsers
-     */
-    private final Map<String, Object> extend = new HashMap<>();
+	/**
+	 * data returned to browsers
+	 */
+	private final Map<String, Object> extend = new HashMap<>();
 
-    /**
-     * retrieve successfully
-     *
-     * @return result including data
-     */
-    public static RestMsg success() {
-        final RestMsg result = new RestMsg();
-        result.setCode(200);
-        result.setMessage("Retrieve success.");
-        return result;
-    }
+	/**
+	 * retrieve successfully
+	 *
+	 * @return result including data
+	 */
+	public static RestMsg success() {
+		final RestMsg result = new RestMsg();
+		result.setCode(200);
+		result.setMessage("Retrieve success.");
+		return result;
+	}
 
-    /**
-     * retrieve failed
-     *
-     * @return result including error message
-     */
-    public static RestMsg failure() {
-        final RestMsg result = new RestMsg();
-        result.setCode(400);
-        result.setMessage("Retrieve failed.");
-        return result;
-    }
+	/**
+	 * retrieve failed
+	 *
+	 * @return result including error message
+	 */
+	public static RestMsg failure() {
+		final RestMsg result = new RestMsg();
+		result.setCode(400);
+		result.setMessage("Retrieve failed.");
+		return result;
+	}
 
-    /**
-     * no args constructor
-     */
-    public RestMsg() {
-    }
-
-    /**
-     * add values with messages
-     *
-     * @param key   the name pattern of value
-     * @param value value
-     * @return RestMsg
-     */
-    public RestMsg add(final String key, final Object value) {
-        this.getExtend().put(key, value);
-        return this;
-    }
+	/**
+	 * add values with messages
+	 *
+	 * @param key   the name pattern of value
+	 * @param value value
+	 * @return RestMsg
+	 */
+	public RestMsg add(final String key, final Object value) {
+		this.getExtend().put(key, value);
+		return this;
+	}
 }
