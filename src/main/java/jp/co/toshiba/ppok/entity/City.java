ead44f0a6c99a9eb8b6e164e8292a893e8be0322
ArcaHozota <arkamahozota@gmail.com>
2023-02-03 13:28:34 +0900

    6.62

--- src/main/java/jp/co/toshiba/ppok/entity/City.java
+++ src/main/java/jp/co/toshiba/ppok/entity/City.java
@@ -2,34 +2,12 @@ package jp.co.toshiba.ppok.entity;
 
 import java.io.Serializable;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.NamedQuery;
-import javax.persistence.Table;
-
-import org.hibernate.annotations.Proxy;
-
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.Data;
 
 /**
  * @author Administrator
  */
-@Entity
-@Getter
-@Setter
-@NoArgsConstructor
-@Table(name = "city")
-@Proxy(lazy = false)
-@NamedQuery(name = "City.removeById", query = "update City c set c.isDeleted = 1 where c.id = :id")
-@NamedQuery(name = "City.getCityInfos", query = "select c from City c where c.isDeleted = 0 order by c.id asc")
-@NamedQuery(name = "City.findByNations", query = "select c from City c where c.countryCode = :nation")
-@NamedQuery(name = "City.getByNations", query = "select c from City c where c.countryCode = :nation order by c.id asc")
-@NamedQuery(name = "City.getByNames", query = "select c from City c where c.name like concat('%', :name, '%') order by c.id asc")
+@Data
 public class City implements Serializable {
 
 	private static final long serialVersionUID = 1815689293387304425L;
@@ -37,43 +15,30 @@ public class City implements Serializable {
 	/**
 	 * This field corresponds to the database column id
 	 */
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 
 	/**
 	 * This field corresponds to the database column name
 	 */
-	@Column(nullable = false)
 	private String name;
 
 	/**
 	 * This field corresponds to the database column country_code
 	 */
-	@Column(name = "country_code", nullable = false)
 	private String countryCode;
 
 	/**
 	 * This field corresponds to the database column district
 	 */
-	@Column(nullable = false)
 	private String district;
 
 	/**
 	 * This field corresponds to the database column population
 	 */
-	@Column(nullable = false)
 	private Long population;
 
 	/**
 	 * This field corresponds to the database column is_deleted
 	 */
-	@Column(name = "is_deleted", nullable = false)
 	private Integer isDeleted;
-
-	@Override
-	public String toString() {
-		return "City [id=" + this.id + ", name=" + this.name + ", countryCode=" + this.countryCode + ", district="
-				+ this.district + ", population=" + this.population + ", isDeleted=" + this.isDeleted + "]";
-	}
 }
--- src/main/java/jp/co/toshiba/ppok/entity/Country.java
+++ src/main/java/jp/co/toshiba/ppok/entity/Country.java
@@ -3,30 +3,12 @@ package jp.co.toshiba.ppok.entity;
 import java.io.Serializable;
 import java.math.BigDecimal;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.NamedQuery;
-import javax.persistence.Table;
-
-import org.hibernate.annotations.Proxy;
-
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.Data;
 
 /**
  * @author Administrator
  */
-@Entity
-@Getter
-@Setter
-@NoArgsConstructor
-@Table(name = "country")
-@Proxy(lazy = false)
-@NamedQuery(name = "Country.findNationCode", query = "select distinct n from Country n where n.name = :name")
+@Data
 public class Country implements Serializable {
 
 	private static final long serialVersionUID = -437505450837045511L;
@@ -34,50 +16,41 @@ public class Country implements Serializable {
 	/**
 	 * This field corresponds to the database column code
 	 */
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
 	private String code;
 
 	/**
 	 * This field corresponds to the database column name
 	 */
-	@Column(nullable = false)
 	private String name;
 
 	/**
 	 * This field corresponds to the database column continent
 	 */
-	@Column(nullable = false)
 	private String continent;
 
 	/**
 	 * This field corresponds to the database column region
 	 */
-	@Column(nullable = false)
 	private String region;
 
 	/**
 	 * This field corresponds to the database column surface_area
 	 */
-	@Column(name = "surface_area", nullable = false)
 	private BigDecimal surfaceArea;
 
 	/**
 	 * This field corresponds to the database column independence_year
 	 */
-	@Column(name = "independence_year")
 	private Integer independenceYear;
 
 	/**
 	 * This field corresponds to the database column population
 	 */
-	@Column(nullable = false)
 	private Long population;
 
 	/**
 	 * This field corresponds to the database column life_expectancy
 	 */
-	@Column(name = "life_expectancy")
 	private Integer lifeExpectancy;
 
 	/**
@@ -88,25 +61,21 @@ public class Country implements Serializable {
 	/**
 	 * This field corresponds to the database column gnp_old
 	 */
-	@Column(name = "gnp_old")
 	private BigDecimal gnpOld;
 
 	/**
 	 * This field corresponds to the database column local_name
 	 */
-	@Column(name = "local_name", nullable = false)
 	private String localName;
 
 	/**
 	 * This field corresponds to the database column government_form
 	 */
-	@Column(name = "government_form", nullable = false)
 	private String governmentForm;
 
 	/**
 	 * This field corresponds to the database column head_of_state
 	 */
-	@Column(name = "head_of_state")
 	private String headOfState;
 
 	/**
@@ -117,22 +86,10 @@ public class Country implements Serializable {
 	/**
 	 * This field corresponds to the database column code2
 	 */
-	@Column(nullable = false)
 	private String code2;
 
 	/**
 	 * This field corresponds to the database column is_deleted
 	 */
-	@Column(name = "is_deleted", nullable = false)
 	private Integer isDeleted;
-
-	@Override
-	public String toString() {
-		return "Country [code=" + this.code + ", name=" + this.name + ", continent=" + this.continent + ", region="
-				+ this.region + ", surfaceArea=" + this.surfaceArea + ", independenceYear=" + this.independenceYear
-				+ ", population=" + this.population + ", lifeExpectancy=" + this.lifeExpectancy + ", gnp=" + this.gnp
-				+ ", gnpOld=" + this.gnpOld + ", localName=" + this.localName + ", governmentForm="
-				+ this.governmentForm + ", headOfState=" + this.headOfState + ", capital=" + this.capital + ", code2="
-				+ this.code2 + ", isDeleted=" + this.isDeleted + "]";
-	}
 }
