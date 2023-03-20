17aa6224427ccea7a0dcfc5344c35f46f681ad35
ArcaHozota <arkamahozota@gmail.com>
2023-03-21 01:59:36 +0900

    8.03

--- src/main/java/jp/co/toshiba/ppok/utils/Pagination.java
+++ src/main/java/jp/co/toshiba/ppok/utils/Pagination.java
@@ -87,6 +87,19 @@ public final class Pagination<T> {
 	/**
 	 * 生成Pagination對象
 	 *
+	 * @param records      結果集
+	 * @param totalRecords 總記錄數
+	 * @param pageNum      當前頁
+	 * @param pageSize     每頁大小
+	 */
+	public static <T> Pagination<T> of(final List<T> records, final int totalRecords, final int pageNum,
+			final int pageSize) {
+		return new Pagination<T>(records, totalRecords, pageNum, pageSize, 5);
+	}
+
+	/**
+	 * 生成Pagination對象
+	 *
 	 * @param records       結果集
 	 * @param totalRecords  總記錄數
 	 * @param pageNum       當前頁
