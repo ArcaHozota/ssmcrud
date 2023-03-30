71dad007538fc66dafeaed078b6f07bc7d40011a
ArcaHozota <arkamahozota@gmail.com>
2023-03-31 08:47:01 +0900

    8.29

--- src/main/java/jp/co/toshiba/ppok/dto/CityDto.java
+++ src/main/java/jp/co/toshiba/ppok/dto/CityDto.java
@@ -1,14 +1,18 @@
 package jp.co.toshiba.ppok.dto;
 
+import java.io.Serializable;
+
 import lombok.Data;
 
 /**
  * Entity of View WORLD_CITY_VIEW
- * 
+ *
  * @author Administrator
  */
 @Data
-public class CityDto {
+public class CityDto implements Serializable {
+
+	private static final long serialVersionUID = -1945579742723628429L;
 
 	/**
 	 * This field corresponds to the database column ID
