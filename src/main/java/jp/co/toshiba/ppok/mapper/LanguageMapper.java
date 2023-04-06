1d0eab22f7ec4b479f6d1a0c929ac6c5b494a666
ArcaHozota <arkamahozota@gmail.com>
2023-04-06 15:49:27 +0900

    8.52

---
+++ src/main/java/jp/co/toshiba/ppok/mapper/LanguageMapper.java
@@ -0,0 +1,13 @@
+package jp.co.toshiba.ppok.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * Mapper of Table WORLD_LANGUAGE
+ *
+ * @author Administrator
+ * @date 2022-12-17
+ */
+@Mapper
+public interface LanguageMapper {
+}
