61e276402b194cca34bbcdbd04160ed3578abd3c
ArcaHozota <arkamahozota@gmail.com>
2023-04-06 15:45:37 +0900

    8.51

--- src/main/java/jp/co/toshiba/ppok/entity/Language.java
+++ src/main/java/jp/co/toshiba/ppok/entity/Language.java
@@ -5,17 +5,34 @@ import java.math.BigDecimal;
 
 import lombok.Data;
 
+/**
+ * Entity of Table WORLD_LANGUAGE
+ *
+ * @author Administrator
+ */
 @Data
 public class Language implements Serializable {
 
 	private static final long serialVersionUID = 487191389336636732L;
 
+	/**
+	 * This field corresponds to the database column COUNTRY_CODE
+	 */
 	private String countryCode;
 
+	/**
+	 * This field corresponds to the database column LANGUAGE
+	 */
 	private String language;
 
+	/**
+	 * This field corresponds to the database column IS_OFFICIAL
+	 */
 	private String isOfficial;
 
+	/**
+	 * This field corresponds to the database column PERCENTAGE
+	 */
 	private BigDecimal percentage;
 
 	/**
