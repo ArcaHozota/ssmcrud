60f6351e8f86be08508f739137564d9154c470e4
Arca Hozota <arkamahozota@gmail.com>
2023-07-14 15:40:17 +0900

    9.25

--- src/main/java/jp/co/toshiba/ppok/dto/CityDto.java
+++ src/main/java/jp/co/toshiba/ppok/dto/CityDto.java
@@ -2,6 +2,7 @@ package jp.co.toshiba.ppok.dto;
 
 import jp.co.toshiba.ppok.entity.CityView;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 /**
  * DTO class of CityInfo
@@ -9,12 +10,13 @@ import lombok.Data;
  * @author Administrator
  */
 @Data
+@EqualsAndHashCode(callSuper = false)
 public class CityDto extends CityView {
 
-    private static final long serialVersionUID = 7478219222594404009L;
+	private static final long serialVersionUID = 7478219222594404009L;
 
-    /**
-     * This field corresponds to the database column LANGUAGE
-     */
-    private String language;
+	/**
+	 * This field corresponds to the database column LANGUAGE
+	 */
+	private String language;
 }
