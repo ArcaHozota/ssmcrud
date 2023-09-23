ad90ad7b2b402ef6458e78c33678667433b51463
Arca Hozota <arkamahozota@gmail.com>
2023-09-23 23:47:06 +0900

    9.58

--- src/main/java/jp/co/toshiba/ppok/utils/Pagination.java
+++ src/main/java/jp/co/toshiba/ppok/utils/Pagination.java
@@ -1,208 +1,205 @@
 package jp.co.toshiba.ppok.utils;
 
+import java.util.Arrays;
 import java.util.List;
 
 import lombok.Getter;
 import lombok.Setter;
 
 /**
- * 數據分頁
+ * 共通ページングクラス
  *
  * @param <T>
  * @author Administrator
- * @since 3.08
  */
 @Getter
 @Setter
 public final class Pagination<T> {
 
 	/**
-	 * 每頁數據
+	 * 毎ページのレコード
 	 */
 	private List<T> records;
 
 	/**
-	 * 當前頁碼
+	 * 当ページ
 	 */
 	private int pageNum;
 
 	/**
-	 * 每頁大小
+	 * ページサイズ
 	 */
 	private int pageSize;
 
 	/**
-	 * 總頁數
+	 * すべてのページ数
 	 */
-	private int totalPages;
+	private long totalPages;
 
 	/**
-	 * 總記錄數
+	 * すべてのレコード数
 	 */
-	private int totalRecords;
+	private long totalRecords;
 
 	/**
-	 * 是否有前一頁
+	 * 前のページはあるか
 	 */
 	private boolean hasPreviousPage = false;
 
 	/**
-	 * 是否有後一頁
+	 * 次のページはあるか
 	 */
 	private boolean hasNextPage = false;
 
 	/**
-	 * 前一頁
+	 * 前のページ
 	 */
 	private int previousPage;
 
 	/**
-	 * 後一頁
+	 * 次のページ
 	 */
 	private int nextPage;
 
 	/**
-	 * 導航條頁碼數量
+	 * ナビゲーションのページ数
 	 */
 	private int navigatePages;
 
 	/**
-	 * 導航條第一頁
+	 * ナビゲーションの最初のページ
 	 */
 	private int naviFirstPage;
 
 	/**
-	 * 導航條最後一頁
+	 * ナビゲーションの最後のページ
 	 */
 	private int naviLastPage;
 
 	/**
-	 * 所有導航頁號碼
+	 * ナビゲーションページの数の集合
 	 */
 	private int[] navigatePageNums;
 
 	/**
-	 * 生成Pagination對象
+	 * Paginationを取得する
 	 *
-	 * @param records      結果集
-	 * @param totalRecords 總記錄數
-	 * @param pageNum      當前頁
+	 * @param records      レコード
+	 * @param totalRecords すべてのレコード数
+	 * @param pageNum      当ページ
 	 */
-	public static <T> Pagination<T> of(final List<T> records, final int totalRecords, final int pageNum) {
-		return new Pagination<>(records, totalRecords, pageNum, 17, 5);
+	public static <T> Pagination<T> of(final List<T> records, final long totalRecords, final int pageNum) {
+		return new Pagination<>(records, totalRecords, pageNum, records.size(), 5);
 	}
 
 	/**
-	 * 生成Pagination對象
+	 * Paginationを取得する
 	 *
-	 * @param records      結果集
-	 * @param totalRecords 總記錄數
-	 * @param pageNum      當前頁
-	 * @param pageSize     每頁大小
+	 * @param records      レコード
+	 * @param totalRecords すべてのレコード数
+	 * @param pageNum      当ページ
+	 * @param pageSize     ページサイズ
 	 */
-	public static <T> Pagination<T> of(final List<T> records, final int totalRecords, final int pageNum,
+	public static <T> Pagination<T> of(final List<T> records, final long totalRecords, final int pageNum,
 			final int pageSize) {
 		return new Pagination<>(records, totalRecords, pageNum, pageSize, 5);
 	}
 
 	/**
-	 * 生成Pagination對象
+	 * Paginationを取得する
 	 *
-	 * @param records       結果集
-	 * @param totalRecords  總記錄數
-	 * @param pageNum       當前頁
-	 * @param pageSize      每頁大小
-	 * @param navigatePages 導航條頁碼數
+	 * @param records       レコード
+	 * @param totalRecords  すべてのレコード数
+	 * @param pageNum       当ページ
+	 * @param pageSize      ページサイズ
+	 * @param navigatePages ナビゲーションのページ数
 	 */
-	public static <T> Pagination<T> of(final List<T> records, final int totalRecords, final int pageNum,
+	public static <T> Pagination<T> of(final List<T> records, final long totalRecords, final int pageNum,
 			final int pageSize, final int navigatePages) {
 		return new Pagination<>(records, totalRecords, pageNum, pageSize, navigatePages);
 	}
 
 	/**
-	 * 封裝Pagination對象
+	 * コンストラクタ
 	 *
-	 * @param records       結果集
-	 * @param totalRecords  總記錄數
-	 * @param pageNum       當前頁
-	 * @param pageSize      每頁大小
-	 * @param navigatePages 導航條頁碼數
+	 * @param records       レコード
+	 * @param totalRecords  すべてのレコード数
+	 * @param pageNum       当ページ
+	 * @param pageSize      ページサイズ
+	 * @param navigatePages ナビゲーションのページ数
 	 */
-	private Pagination(final List<T> records, final int totalRecords, final int pageNum, final int pageSize,
+	private Pagination(final List<T> records, final long totalRecords, final int pageNum, final int pageSize,
 			final int navigatePages) {
 		if (records != null && !records.isEmpty()) {
 			this.pageNum = pageNum;
 			this.records = records;
-			this.pageSize = records.size();
+			this.pageSize = pageSize;
 			this.totalRecords = totalRecords;
-			final int ape = this.totalRecords / pageSize;
+			final long ape = this.totalRecords / pageSize;
 			this.totalPages = this.totalRecords % pageSize == 0 ? ape : ape + 1;
 		} else if (records != null) {
 			this.pageNum = 1;
 			this.records = null;
 			this.pageSize = 0;
-			this.totalRecords = 0;
-			this.totalPages = 1;
+			this.totalRecords = 0L;
+			this.totalPages = 1L;
 		} else {
-			throw new PaginationException("數據集合類型錯誤！");
+			throw new PaginationException("データのコレクションは間違いました。");
 		}
 		this.calcByNavigatePages(navigatePages);
 	}
 
 	/**
-	 * 根據導航條頁數進行計算
+	 * ナビゲーションのページ数によって色んな計算処理を行う
 	 *
-	 * @param navigatePages 導航條頁碼數量
+	 * @param navigatePages ナビゲーションのページ数
 	 */
 	private void calcByNavigatePages(final int navigatePages) {
-		// 設置導航條頁碼數量
+		// ナビゲーションのページ数を設定する
 		this.setNavigatePages(navigatePages);
-		// 計算導航頁
-		this.calcNavigatepageNums();
-		// 計算前後頁，第一頁以及最後頁
+		// ナビゲーションページの数の集合を取得する
+		this.calcNavigatePageNums();
+		// 前のページ、次のページ、最初及び最後のページを取得する
 		this.calcPage();
-		// 判定頁面邊界
+		// ページングエッジを判断する
 		this.discernPageBoundary();
 	}
 
 	/**
-	 * 計算導航頁
+	 * ナビゲーションページの数の集合を取得する
 	 */
-	private void calcNavigatepageNums() {
-		// 當總頁數小於或等於導航條頁碼數時
+	private void calcNavigatePageNums() {
 		if (this.totalPages <= this.navigatePages) {
-			this.navigatePageNums = new int[this.totalPages];
+			this.navigatePageNums = new int[(int) this.totalPages];
 			for (int i = 0; i < this.totalPages; i++) {
 				this.navigatePageNums[i] = i + 1;
 			}
 			return;
 		}
-		// 當總頁數大於導航條頁碼數時
 		this.navigatePageNums = new int[this.navigatePages];
 		int startNum = this.pageNum - this.navigatePages / 2;
 		int endNum = this.pageNum + this.navigatePages / 2;
-		if (startNum < 1) {
-			startNum = 1;
-			// 第一導航條頁
-			for (int i = 0; i < this.navigatePages; i++) {
-				this.navigatePageNums[i] = startNum++;
-			}
-		} else if (endNum > this.totalPages) {
-			endNum = this.totalPages;
-			// 最後導航條頁
+		if (endNum > this.totalPages && startNum >= 1) {
+			endNum = (int) this.totalPages;
+			// 最後のナビゲーションページ
 			for (int i = this.navigatePages - 1; i >= 0; i--) {
-				this.navigatePageNums[i] = endNum--;
+				this.navigatePageNums[i] = endNum;
+				endNum--;
 			}
 		} else {
-			// 所有中間頁
+			if (startNum < 1) {
+				startNum = 1;
+			}
+			// 他のナビゲーションページ
 			for (int i = 0; i < this.navigatePages; i++) {
-				this.navigatePageNums[i] = startNum++;
+				this.navigatePageNums[i] = startNum;
+				startNum++;
 			}
 		}
 	}
 
 	/**
-	 * 計算前後頁，第一頁以及最後頁
+	 * 前のページ、次のページ、最初及び最後のページを取得する
 	 */
 	private void calcPage() {
 		if (this.navigatePageNums != null && this.navigatePageNums.length > 0) {
@@ -218,7 +215,7 @@ public final class Pagination<T> {
 	}
 
 	/**
-	 * 判定頁面邊界
+	 * ページングエッジを判断する
 	 */
 	private void discernPageBoundary() {
 		this.hasPreviousPage = this.pageNum > 1;
@@ -226,12 +223,28 @@ public final class Pagination<T> {
 	}
 
 	/**
-	 * 是否包含内容
+	 * 内容はあるかどうかを判断する
 	 */
 	public boolean hasContent() {
 		return !this.records.isEmpty();
 	}
 
+	/**
+	 * toString
+	 */
+	@Override
+	public String toString() {
+		return "Pagination [records=" + this.records + ", pageNum=" + this.pageNum + ", pageSize=" + this.pageSize
+				+ ", totalPages=" + this.totalPages + ", totalRecords=" + this.totalRecords + ", hasPrePage="
+				+ this.hasPreviousPage + ", hasNextPage=" + this.hasNextPage + ", previousPage=" + this.previousPage
+				+ ", nextPage=" + this.nextPage + ", navigatePages=" + this.navigatePages + ", naviFirstPage="
+				+ this.naviFirstPage + ", naviLastPage=" + this.naviLastPage + ", navigatePageNums="
+				+ Arrays.toString(this.navigatePageNums) + "]";
+	}
+
+	/**
+	 * 例外
+	 */
 	static class PaginationException extends RuntimeException {
 
 		private static final long serialVersionUID = 4906724781325178457L;
@@ -239,6 +252,5 @@ public final class Pagination<T> {
 		public PaginationException(final String message) {
 			super(message);
 		}
-
 	}
 }
