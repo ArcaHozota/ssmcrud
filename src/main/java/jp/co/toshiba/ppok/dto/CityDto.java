2ba17eaa8963bd37fb900c618817553b50a042e2
Arca Hozota <arkamahozota@gmail.com>
2023-09-28 10:11:11 +0900

    9.96

--- src/main/java/jp/co/toshiba/ppok/dto/CityDto.java
+++ src/main/java/jp/co/toshiba/ppok/dto/CityDto.java
@@ -1,18 +1,11 @@
 package jp.co.toshiba.ppok.dto;
 
-import jp.co.toshiba.ppok.entity.CityView;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-
 /**
  * 都市情報DTO
  *
  * @author ArcaHozota
  * @since 6.24
  */
-@Data
-@EqualsAndHashCode(callSuper = false)
-public final class CityDto extends CityView {
-
-	private static final long serialVersionUID = -4246591226136439978L;
+public record CityDto(Long id, String name, String continent, String nation, String district, Long population,
+		String language) {
 }
