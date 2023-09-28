a1391315d34c797dc173d91de2fae44f10778af7
Arca Hozota <arkamahozota@gmail.com>
2023-09-28 10:09:03 +0900

    9.95

--- src/main/java/jp/co/toshiba/ppok/controller/IndexController.java
+++ src/main/java/jp/co/toshiba/ppok/controller/IndexController.java
@@ -2,10 +2,10 @@ package jp.co.toshiba.ppok.controller;
 
 import java.io.IOException;
 
-import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import jakarta.servlet.http.HttpServletResponse;
 
 /**
  * インデクスコントローラ
