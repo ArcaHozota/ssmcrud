22b81b28fde2405885e30c21aa13dfec6fae1568
ArkamaHozota <arkamahozota@gmail.com>
2023-10-17 14:55:21 +0900

    11.40

--- src/main/java/jp/co/toshiba/ppok/config/WebMvcConfig.java
+++ src/main/java/jp/co/toshiba/ppok/config/WebMvcConfig.java
@@ -6,13 +6,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 import jp.co.toshiba.ppok.utils.Messages;
 import lombok.extern.log4j.Log4j2;
 
 /**
- * SpringMVC配置クラス
+ * SpringMVC設定クラス
  *
  * @author ArcaHozota
  * @since 1.64
@@ -22,23 +23,7 @@ import lombok.extern.log4j.Log4j2;
 public class WebMvcConfig extends WebMvcConfigurationSupport {
 
 	/**
-	 * SpringMVCフレームワークを拡張するメッセージ・コンバーター
-	 *
-	 * @param converters コンバーター
-	 */
-	@Override
-	protected void extendMessageConverters(final List<HttpMessageConverter<?>> converters) {
-		log.info(Messages.MSG001);
-		// 創建消息轉換器對象；
-		final MappingJackson2HttpMessageConverter messageConverter = new MappingJackson2HttpMessageConverter();
-		// 設置對象轉換器，底層使用Jackson將Java對象轉為JSON；
-		messageConverter.setObjectMapper(new JacksonObjectMapper());
-		// 將上述消息轉換器追加到SpringMVC框架的轉換器容器中；
-		converters.add(0, messageConverter);
-	}
-
-	/**
-	 * 設置靜態資源映射
+	 * 静的リソースマッピングを設定する
 	 *
 	 * @param registry 注冊説明
 	 */
@@ -47,5 +32,31 @@ public class WebMvcConfig extends WebMvcConfigurationSupport {
 		log.info(Messages.MSG002);
 		registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
 		registry.addResourceHandler("/public/**").addResourceLocations("classpath:/public/");
+	}
+
+	/**
+	 * ビューコントローラを定義する
+	 *
+	 * @param registry
+	 */
+	@Override
+	public void addViewControllers(final ViewControllerRegistry registry) {
+		registry.addRedirectViewController("/index", "/public/cities.html");
+	}
+
+	/**
+	 * SpringMVCフレームワークを拡張するメッセージ・コンバーター
+	 *
+	 * @param converters コンバーター
+	 */
+	@Override
+	protected void extendMessageConverters(final List<HttpMessageConverter<?>> converters) {
+		log.info(Messages.MSG001);
+		// メッセージコンバータオブジェクトを作成する。
+		final MappingJackson2HttpMessageConverter messageConverter = new MappingJackson2HttpMessageConverter();
+		// オブジェクトコンバータを設定し、Jacksonを使用してJavaオブジェクトをJSONに変換します。
+		messageConverter.setObjectMapper(new JacksonObjectMapper());
+		// 上記のメッセージコンバータをSpringMVCフレームワークのコンバータコンテナに追加します。
+		converters.add(0, messageConverter);
 	}
 }
